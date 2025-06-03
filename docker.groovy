98c8768b7da75ab4b99d2a2b2bca3bc0403be198
Vikas Ghandge <ghandgevikas804@gmail.com>
2025-06-04 02:39:38 +0530

    Update docker.groovy

--- docker.groovy
+++ docker.groovy
@@ -6,10 +6,8 @@ pipeline {
         maven 'maven3'
     }
 
-    environments {
-
+    environment {
         SCANNER_HOME = tool 'sonar-scanner'
-
     }
 
     stages {
@@ -18,66 +16,86 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Vikasghandge/Full-Stack-App.git'
             }
         }
-        // stage('Build Code') {
-        //     steps {
 
+        stage('Build with Maven') {
+            steps {
+                dir('FullStack-Blogging-App-main') {
+                    sh 'mvn clean package'
+                }
+            }
+        }
+
+        // stage('SonarQube Analysis') {
+        //     steps {
+        //           dir('FullStack-Blogging-App-main') {
+        //              withSonarQubeEnv('sonar-server') {
+        //                 sh """
+        //                   ${SCANNER_HOME}/bin/sonar-scanner \
+        //                      -Dsonar.projectKey=Hotstar \
+        //                      -Dsonar.project.name=Hotstar \
+        //                      -Dsonar.sources=. \
+        //                      -Dsonar.java.binaries=target/classes \
+        //                      -X
+        //                      """
+        //                           }
+        //                     }
+        //                 }
+        //     }
+
+
+        // stage('Quality Gate') {
+        //     steps {
+        //         script {
+        //             waitForQualityGate abortPipeline: true, credentialsId: 'Sonar-token'
+        //         }
         //     }
         // }
-        stage('SonarQube Analysis') {
-            steps {
-                withSonarQubeEnv('sonar-server') {
-                    sh '''${SCANNER_HOME}/bin/sonar-scanner \
-                    -Dsonar.projectName=Hotstar \
-                    -Dsonar.projectKey=Hotstar'''
-                }
-            }
-        }
-        stage('Quality Gate') {
-            steps {
-                script {
-                    waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'
-                }
-            }
-        }
-        stage('Docker Scout FS') {
-            steps {
-                script {
-                    withDockerRegistry(credentialsId: 'docker', toolName: 'docker') {
-                        sh 'docker-scout quickview fs://.'
-                        sh 'docker-scout cves fs://.'
-                    }
-                }
-            }
-        }
+
+        // stage('Docker Scout FS') {
+        //     steps {
+        //         script {
+        //             // Login to Docker Hub before running docker-scout on fs
+        //             withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        //                 sh 'echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin'
+        //             }
+        //             sh 'docker-scout quickview fs://.'
+        //             sh 'docker-scout cves fs://.'
+        //         }
+        //     }
+        // }
+
         stage('Docker Build & Push') {
-            steps {
-                dir('Hotstar-Clone-main') {
-                    script {
-                        withDockerRegistry(credentialsId: 'docker', toolName: 'docker') {
-                            sh "docker build -t full-stack ."
-                            sh "docker tag full-stack ghandgevikas/full-stack:latest"
-                            sh "docker push full-stack"
-                        }
-                    }
+          steps {
+             dir('FullStack-Blogging-App-main') {
+                  script {
+                      withDockerRegistry(credentialsId: 'docker') {
+                        sh 'docker build -t full-stack .'
+                        sh 'docker tag full-stack ghandgevikas/full-stack:latest'
+                        sh 'docker push ghandgevikas/full-stack:latest'
                 }
             }
         }
+    }
+}
+
+
         stage('Docker Scout Image') {
             steps {
                 script {
-                    withDockerRegistry(credentialsId: 'docker', toolName: 'docker') {
-                        sh "docker-scout quickview full-stack"
-                        sh "docker-scout cves full-stack"
-                        sh "docker-scout recommendations full-stack"
+                    // Login to Docker Hub before running docker-scout on image
+                    withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh 'echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin'
                     }
+                    sh 'docker-scout quickview full-stack'
+                    sh 'docker-scout cves full-stack'
+                    sh 'docker-scout recommendations full-stack'
                 }
             }
         }
+
         stage('Deploy Locally Container') {
             steps {
-                dir('') {
-                    sh 'docker run -d --name full-stack-app -p 8080:8080 full-stack'
-                }
+                sh 'docker run -d --rm --name full-stack-app -p 8081:8080 full-stack'
             }
         }
     }
